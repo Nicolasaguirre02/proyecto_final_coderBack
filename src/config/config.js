c398fd8b8190274323785f113fc7860e58717167
nicolas aguirre <nico.aguirre02@hotmail.com>
2024-09-23 19:03:02 -0300

    instalaciones

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 export const config = {
-    port: process.env.PORT,
+    port: process.env.PORT || 8080,
     mongoUrl: process.env.MONGO_URL,
     nodeEnv: process.env.NODE_ENV 
 }
